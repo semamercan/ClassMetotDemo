4fec4549ab846bcce63dd6ebd1c20f398a04501c
sema mercan <sema.mercan06@gmail.com>
2021-01-23 04:13:31 +0300

    16 Ocak Dersi - Ödev 3

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri) //musteri parametreleri kullanılacak
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " eklenmiştir.");
+        }
+
+        public void Listele(Musteri[] musteriler)
+        {
+            Console.WriteLine("\nTüm Liste:");
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id + ":" + musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " " + musteri.TelNo);
+            }
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("\n" + musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " silinmiştir.");
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,36 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri(); //instance
+            musteri1.Id = 1;
+            musteri1.MusteriAdi = "Sema";
+            musteri1.MusteriSoyadi = "Mercan";
+            musteri1.TelNo = "05555555555";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.MusteriAdi = "Beyza";
+            musteri2.MusteriSoyadi = "Mercan";
+            musteri2.TelNo = "05553333333";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            foreach (Musteri musteri in musteriler) //musteriler arrayında döner. musteri her müşterinin takma ismidir.
+            {
+                Console.WriteLine(musteri.MusteriAdi);
+                Console.WriteLine(musteri.MusteriSoyadi);
+                Console.WriteLine(musteri.TelNo);
+                Console.WriteLine("----------------------");
+            }
+
+            MusteriManager musteriManager = new MusteriManager(); //instance
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Listele(musteriler);
+            musteriManager.Sil(musteri1);
+           
+
+
         }
     }
 }
